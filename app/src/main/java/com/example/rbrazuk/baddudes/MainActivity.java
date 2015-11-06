b07816e6976193077ed9400a4ae2415e9904db83
Ross Brazuk <rbrazuk@tonicdesign.com>
2015-11-06 18:52:49 -0500

    First test in progress

--- app/src/main/java/com/example/rbrazuk/baddudes/MainActivity.java
+++ app/src/main/java/com/example/rbrazuk/baddudes/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.rbrazuk.baddudes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void toast(View view){
-        Toast.makeText(getApplicationContext(),"The president has been kidnapped by ninjas!",Toast.LENGTH_LONG).show();
+
+        TextView messageLabel = (TextView) findViewById(R.id.messageLabel);
+        String message = "Let's go for a burger...HA! HA! HA!";
+
+        messageLabel.setText(message);
+
+        // Toast.makeText(getApplicationContext(),"The president has been kidnapped by ninjas!",Toast.LENGTH_LONG).show();
     }
 
     @Override
